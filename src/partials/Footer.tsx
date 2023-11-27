c8dc0fa2cf47328ea67e220babfe9fc62f0d3169
Bob Lin <bob401710@gmail.com>
2023-11-27 11:08:31 +0800

    Remove ProjectList

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,6 +1,28 @@
-import { FooterCopyright, Section } from 'astro-boilerplate-components';
+import { Section } from 'astro-boilerplate-components';
 
 import { AppConfig } from '@/utils/AppConfig';
+
+type IFooterCopyrightProps = {
+  site_name: string;
+};
+
+const FooterCopyright = (props: IFooterCopyrightProps) => (
+  <div className="border-t border-gray-600 pt-5">
+    <div className="text-sm text-gray-200">
+      © Copyright {new Date().getFullYear()} by {props.site_name}. Built with ♥
+      by{' '}
+      <a
+        className="text-cyan-400 hover:underline"
+        href="https://github.com/169"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        @169
+      </a>
+      .
+    </div>
+  </div>
+);
 
 const Footer = () => (
   <Section>
--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
+  site_name: 'Astro Boilerplate',
   title: 'Astro boilerplate',
   description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  author: 'Bob',
   locale_region: 'en-us',
   locale: 'en',
 };
