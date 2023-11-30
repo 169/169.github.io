a286b45b3900c25ed248e3f1e9e250cc2f157a21
Bob Lin <bob401710@gmail.com>
2023-11-30 15:42:37 +0800

    Add giscus as comment sys

--- src/partials/BlogPost.tsx
+++ src/partials/BlogPost.tsx
@@ -14,6 +14,22 @@ const BlogPost = (props: IBlogPostProps) => (
     <PostHeader content={props.frontmatter} author={AppConfig.author} />
 
     <PostContent content={props.frontmatter}>{props.children}</PostContent>
+
+    <script src="https://giscus.app/client.js"
+      data-repo="169/169.github.io"
+      data-repo-id="R_kgDOKyYnfg"
+      data-category="General"
+      data-category-id="DIC_kwDOKyYnfs4CbYd8"
+      data-mapping="pathname"
+      data-strict="0"
+      data-reactions-enabled="1"
+      data-emit-metadata="0"
+      data-input-position="top"
+      data-theme="preferred_color_scheme"
+      data-lang="en"
+      crossorigin="anonymous"
+      async>
+    </script>
   </Section>
 );
 
